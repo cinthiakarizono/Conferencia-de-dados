8458b6d086ae26ad37ef384fa469e235be4cc606
Cinthia Kazue Arizono <carizono@gmail.com>
2023-02-12 01:41:12 -0300

    Convertendo os arquivos

--- conferencia.py
+++ conferencia.py
@@ -2,6 +2,8 @@
 import pandas as pd
 #converte arquivos pdf em csv
 import tabula
-drive = pd.read_excel("arquivos\DRIVE.xlsx", None)
+sistema = tabula.read_pdf("arquivos/sistema.pdf", pages = "all")
+print(sistema)
+read = pd.read_excel("arquivos/drive.xlsx")
+read.to_csv("arquivos/drive.csv", index = None, header = True)
 
-drive
