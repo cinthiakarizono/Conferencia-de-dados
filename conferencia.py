0eae12d5d3a7e51e966aacdc0f47928ac2eb6217
Cinthia Kazue Arizono <carizono@gmail.com>
2023-02-12 23:00:27 -0300

    Implementando as conferencias

--- conferencia.py
+++ conferencia.py
@@ -1,9 +1,17 @@
 #ferramenta para trabalhar dados
 import pandas as pd
-#converte arquivos pdf em csv
 import tabula
-sistema = tabula.read_pdf("arquivos/sistema.pdf", pages = "all")
-print(sistema)
-read = pd.read_excel("arquivos/drive.xlsx")
-read.to_csv("arquivos/drive.csv", index = None, header = True)
+from tabula import read_pdf
+
+#convertendo pdf em csv
+tabula.convert_into("arquivos\sistema.pdf", "arquivos\sistema.csv", output_format = "csv", pages = "all")
+sistema = tabula.read_pdf("arquivos\sistema.pdf", pages = "all")
+print(type(sistema))
+#convertendo arquivo excel em csv
+drive = pd.read_excel("arquivos/drive.xlsx")
+print(type(drive))
+#tabela com as diferenças entre as bases de dados
+diff = pd.merge("arquivos\sistema.csv",drive, on = "RG, NOME, CONVENIO, VALOR", how = "outer")
+conferencia = diff.loc[lambda x: x["meege"] != "both"]
+print(conferencia)
 
