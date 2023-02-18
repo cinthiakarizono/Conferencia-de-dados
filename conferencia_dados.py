d1eb336b4f2039873b1bdf4cdc8165f70a3a5c8d
Cinthia Kazue Arizono <carizono@gmail.com>
2023-02-17 22:06:49 -0300

    Correções

--- conferencia_dados.py
+++ conferencia_dados.py
@@ -4,13 +4,13 @@ import tabula
 from tabula import read_pdf
 
 #convertendo pdf em csv
-tabula.convert_into("arquivos\sistema.pdf", "arquivos\sistema.csv", output_format = "csv", pages = "all")
-sistema = tabula.read_pdf("arquivos\sistema.pdf", pages = "all")
+tabula.convert_into("dados\sistema .pdf", "dados\sistema.csv", output_format = "csv", pages = "all")
+sistema = tabula.read_pdf("dados\sistema .pdf", pages = "all")
 
 #convertendo arquivo excel em csv
-drive = pd.read_excel("arquivos/drive.xlsx")
+drive = pd.read_excel("dados/drive.xlsx")
 print(type(drive))
 #tabela com as diferenças entre as bases de dados
-diff = pd.merge("arquivos\sistema.csv",drive, on = "RG, NOME, CONVENIO, VALOR", how = "outer")
+diff = pd.merge("dados\sistema.csv",drive, on = "RG, NOME, CONVENIO, VALOR", how = "outer")
 conferencia = diff.loc[lambda x: x["merge"] != "both"]
 print(conferencia)
